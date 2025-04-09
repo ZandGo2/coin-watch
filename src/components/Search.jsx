25b2991e314199d1a86b62c0e1358da59da249c8
“[Mobin <[zandgo2@gmail.com]>
2025-04-09 16:23:22 +0330

    make loading

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,21 +1,25 @@
 import React, { useEffect, useState } from "react";
 import styles from "./search.module.css";
 import { SearchCoin } from "../services/FechApi";
+import { Commet } from "react-loading-indicators";
 import ListSearchCoin from "./ListSearchCoin";
 
 const Search = ({ selectHandler }) => {
   const [data, setData] = useState([]);
   const [inputData, setinputData] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const changeHandler = (e) => {
     setinputData(e.target.value);
   };
 
   useEffect(() => {
+    setLoading(true);
     const getDataSearch = async () => {
       const res = await SearchCoin(inputData);
       const json = await res.json();
       setData(json.coins);
+      setLoading(false);
     };
     getDataSearch();
   }, [inputData]);
@@ -36,9 +40,18 @@ const Search = ({ selectHandler }) => {
       </div>
       {!!inputData && (
         <div className={styles.containerSearchListCoin}>
-          {data.map((item) => (
-            <ListSearchCoin key={item.id} data={item} />
-          ))}
+          {loading && (
+            <div className={styles.loading}>
+              <Commet
+                color="#2a4cd0"
+                size="medium"
+                text="Loading"
+                textColor=""
+              />
+            </div>
+          )}
+          {!loading &&
+            data.map((item) => <ListSearchCoin key={item.id} data={item} />)}
         </div>
       )}
     </>
--- src/templates/HomePage.jsx
+++ src/templates/HomePage.jsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from "react";
-
 import { ListAllCoin } from "../services/FechApi";
-
+import { Commet } from "react-loading-indicators";
 import styles from "../components/listCoin.module.css";
-
 import ListCoin from "../components/ListCoin";
 import PageNation from "../components/PageNation";
 import Search from "../components/Search";
@@ -12,12 +10,15 @@ const HomePage = () => {
   const [ListCion, setListCion] = useState([]);
   const [page, setPage] = useState(1);
   const [currency, setCurrency] = useState("usd");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     const getData = async () => {
       const res = await ListAllCoin(page, currency);
       const json = await res.json();
       setListCion(json);
+      setLoading(false);
     };
     getData();
   }, [page, currency]);
@@ -48,9 +49,15 @@ const HomePage = () => {
         </div>
         <span>Chart</span>
       </div>
-      {ListCion.map((item) => (
-        <ListCoin key={item.id} data={item} currency={currency} />
-      ))}
+      {loading && (
+        <div className={styles.loading}>
+          <Commet color="#2a4cd0" size="medium" text="Loading" textColor="" />
+        </div>
+      )}
+      {!loading &&
+        ListCion.map((item) => (
+          <ListCoin key={item.id} data={item} currency={currency} />
+        ))}
       <PageNation page={page} PageHandler={PageHandler} />
     </main>
   );
